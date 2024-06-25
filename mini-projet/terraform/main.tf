cb874b260913aed79269c54abd71756109f41e71
Franck Kouam <kouamfj@gmail.com>
2024-06-25 17:26:14 +0000

    Removing redundant security group rules and setting the rights IP adresses

--- mini-projet/terraform/main.tf
+++ mini-projet/terraform/main.tf
@@ -109,12 +109,6 @@ resource "aws_security_group" "frontend_security_group" {
     to_port           = 80
   }
 
-  ingress {
-    cidr_blocks = [aws_subnet.private.cidr_block]
-    from_port   = 0
-    protocol    = "icmp"
-    to_port     = 0
-  }
 
   egress {
     cidr_blocks         = ["0.0.0.0/0"]
@@ -148,16 +142,9 @@ resource "aws_security_group" "streamer_security_group" {
     to_port           = 22
   }
 
-  ingress {
-    cidr_blocks         = ["0.0.0.0/0"]
-    from_port         = 1935
-    protocol       = "tcp"
-    to_port           = 1935
-  }
-
   
   ingress {
-    cidr_blocks = [aws_subnet.private.cidr_block]
+    cidr_blocks = [aws_subnet.public.cidr_block]
     from_port   = 0
     protocol    = "-1"
     to_port     = 0
